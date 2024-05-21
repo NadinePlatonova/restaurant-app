488f11f7cab5259e7ae619ff7a1b24f2b079b5ef
Nadezhda Platonova <platonova.nadezhda.webdev@yandex.ru>
2024-05-22 01:46:08 +0300

    feat: add Orders page and fix: change footer and cart icon

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-orange-500 flex items-center justify-between">
-      <Link href="/" className="font-bold text-sm md:text-xl uppercase">Pizza Pazza</Link>
+    <div className="h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-orange-500 flex items-center justify-between">
+      <Link href="/" className="font-bold text-xl uppercase">Pizza Pazza</Link>
       <p className="text-sm uppercase">© All rights reserved.</p>
     </div>
   )
