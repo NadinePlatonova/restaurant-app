9ceb39eb19657d0a0e54f7850a7a1eb0f5190a42
Nadezhda Platonova <platonova.nadezhda.webdev@yandex.ru>
2024-05-22 00:08:34 +0300

    feat: add Product and Cart pages, fix: change menu and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className="h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-orange-500 flex items-center justify-between">
-      <Link href="/" className="font-bold text-xl uppercase">Pizza Pazza</Link>
-      <p className="uppercase">© All rights reserved.</p>
+      <Link href="/" className="font-bold text-sm md:text-xl uppercase">Pizza Pazza</Link>
+      <p className="text-sm uppercase">© All rights reserved.</p>
     </div>
   )
 }
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -19,9 +19,9 @@ const Menu = () => {
   return (
     <div>
       {!open ? (
-        <Image src="/open.png" alt="" className="w-auto h-auto" width={20} height={20} onClick={() => setOpen(true)}/>
+        <Image src="/open.png" alt="" className="w-auto h-auto" priority width={20} height={20} onClick={() => setOpen(true)}/>
       ) : (
-        <Image src="/close.png" alt="" className="w-auto h-auto" width={20} height={20} onClick={() => setOpen(false)}/>
+        <Image src="/close.png" alt="" className="w-auto h-auto" priority width={20} height={20} onClick={() => setOpen(false)}/>
       )}
       {open && (
         <div className="bg-orange-500 text-white absolute left-0 top-24 w-full h-[calc(100vh-6rem)] flex flex-col gap-8 items-center justify-center text-3xl z-10">
