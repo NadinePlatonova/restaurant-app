2bdb9f3837a18154ef4d0c181c328bde27dab7df
Nadezhda Platonova <platonova.nadezhda.webdev@yandex.ru>
2024-05-22 18:09:57 +0300

    fix: change button color in menu and format the code

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 const OrdersPage = () => {
   return (
@@ -18,27 +18,33 @@ const OrdersPage = () => {
             <td className="hidden md:block py-6 px-1">123234354546</td>
             <td className="py-6 px-1">19.07.2023</td>
             <td className="py-6 px-1">89.90</td>
-            <td className="hidden md:block py-6 px-1">Big Burger enu (2), Veggy Pizza (2), Coca Cola 1L (2)</td>
+            <td className="hidden md:block py-6 px-1">
+              Big Burger enu (2), Veggy Pizza (2), Coca Cola 1L (2)
+            </td>
             <td className="py-6 px-1">On the way (approx. 10min)...</td>
           </tr>
           <tr className="text-sm md:text-base odd:bg-gray-100">
             <td className="hidden md:block py-6 px-1">123234354546</td>
             <td className="py-6 px-1">19.07.2023</td>
             <td className="py-6 px-1">89.90</td>
-            <td className="hidden md:block py-6 px-1">Big Burger enu (2), Veggy Pizza (2), Coca Cola 1L (2)</td>
+            <td className="hidden md:block py-6 px-1">
+              Big Burger enu (2), Veggy Pizza (2), Coca Cola 1L (2)
+            </td>
             <td className="py-6 px-1">On the way (approx. 10min)...</td>
           </tr>
           <tr className="text-sm md:text-base odd:bg-gray-100">
             <td className="hidden md:block py-6 px-1">123234354546</td>
             <td className="py-6 px-1">19.07.2023</td>
             <td className="py-6 px-1">89.90</td>
-            <td className="hidden md:block py-6 px-1">Big Burger enu (2), Veggy Pizza (2), Coca Cola 1L (2)</td>
+            <td className="hidden md:block py-6 px-1">
+              Big Burger enu (2), Veggy Pizza (2), Coca Cola 1L (2)
+            </td>
             <td className="py-6 px-1">On the way (approx. 10min)...</td>
           </tr>
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default OrdersPage
+export default OrdersPage;
--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,7 +1,7 @@
-import Price from '@/components/Price'
-import { singleProduct } from '@/components/data'
-import Image from 'next/image'
-import React from 'react'
+import Price from "@/components/Price";
+import { singleProduct } from "@/components/data";
+import Image from "next/image";
+import React from "react";
 
 const SingleProductPage = () => {
   return (
@@ -9,17 +9,30 @@ const SingleProductPage = () => {
       {/* IMAGE CONTAINER */}
       {singleProduct.img && (
         <div className="relative w-full h-1/2 md:h-[70%]">
-          <Image src={singleProduct.img} alt="" className="object-contain" fill sizes="100%" priority/>
+          <Image
+            src={singleProduct.img}
+            alt=""
+            className="object-contain"
+            fill
+            sizes="100%"
+            priority
+          />
         </div>
       )}
       {/* TEXT CONTAINER */}
       <div className="h-1/2 flex flex-col gap-4 md:h-[70%] md:justify-center md:gap-6 xl:gap-8">
-        <h1 className="text-3xl font-bold uppercase xl:text-5xl">{singleProduct.title}</h1>
+        <h1 className="text-3xl font-bold uppercase xl:text-5xl">
+          {singleProduct.title}
+        </h1>
         <p>{singleProduct.desc}</p>
-        <Price price={singleProduct.price} id={singleProduct.id} options={singleProduct.options}/>
+        <Price
+          price={singleProduct.price}
+          id={singleProduct.id}
+          options={singleProduct.options}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SingleProductPage
+export default SingleProductPage;
